ef0061268e13a841f169f4ebb74e5740029edfdf
M-UmarJaved <muhammadumarjaved1325@gmail.com>
2024-09-22 20:48:18 +0500

    Task05.cpp

--- Task05.cpp
+++ Task05.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 main(){
-string person;
+string person_Name;
 int target_weight;
 int days;
 cout<< " Enter the Name of the person: ";
-cin >> person;
+cin >> person_Name;
 cout<< " Enter the target weight loss in kilgrams: ";
 cin >> target_weight;
 days = target_weight * 15;
-cout<< person<<" will need "<< days<< " days to lose "<< target_weight<<" kg of weight by following the doctor's suggestions";
+cout<< person_Name<<" will need "<< days<< " days to lose "<< target_weight<<" kg of weight by following the doctor's suggestions";
 }

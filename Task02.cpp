a0853f43113dbcc2c994600162364e02b15cb272
M-UmarJaved <muhammadumarjaved1325@gmail.com>
2024-09-22 20:46:01 +0500

    Task02.cpp

--- Task02.cpp
+++ Task02.cpp
@@ -3,12 +3,12 @@ using namespace std;
 main(){
 int minutes;
 int frames;
-int total;
+int total_Frames;
 cout<< " Number of Minutes: ";
 cin >> minutes;
 cout<< " Frames per second: ";
 cin >> frames;
 
-total = (minutes*60)*frames;
-cout<<" Total number of Frames: "<< total;
+total_Frames = (minutes*60)*frames;
+cout<<" Total number of Frames: "<< total_Frames;
 }
